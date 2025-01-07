f6dfd1fec45f3cd486b2e7f265f37572d1356ac0
Nakul <nakul.it94@gmail.com>
2025-01-07 21:52:39 +0530

    changes

---
+++ Add Two Numbers/solutions.php
@@ -0,0 +1,32 @@
+<?php
+class Solution {
+
+    /**
+     * @param ListNode $l1
+     * @param ListNode $l2
+     * @return ListNode
+     */
+    function addTwoNumbers($l1, $l2) {
+        // $n = count($words);
+        // $prefix = array_fill(0, $n + 1, 0); 
+
+        // for ($i = 0; $i < $n; $i++) {
+        //     if (in_array($words[$i][0], $vowels) && in_array($words[$i][strlen($words[$i]) - 1], $vowels)) {
+        //         $prefix[$i + 1] = $prefix[$i] + 1;
+        //     } else {
+        //         $prefix[$i + 1] = $prefix[$i];
+        //     }
+        // }
+
+        // $result = [];
+
+        // foreach ($queries as $query) {
+        //     $li = $query[0];
+        //     $ri = $query[1];
+
+        //     $result[] = $prefix[$ri + 1] - $prefix[$li];
+        // }
+
+        // return $result;
+    }
+}
